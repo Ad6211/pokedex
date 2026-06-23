d3bdc42d98593fdd0b676305e4dfa761f23c8648
Ad6211 <adnojojo@gmail.com>
2026-06-23 16:57:16 +0200

    couleur boutons

--- app.js
+++ app.js
@@ -52,9 +52,28 @@ async function chargerFiltres() {
   try {
     const response = await fetch("https://pokeapi.co/api/v2/type");
     const alltypes = await response.json();
-
+    const couleurs = {
+      normal: "#A8A77A",
+      fire: "#EE8130",
+      water: "#6390F0",
+      electric: "#F7D02C",
+      grass: "#7AC74C",
+      ice: "#96D9D6",
+      fighting: "#C22E28",
+      poison: "#A33EA1",
+      ground: "#E2BF65",
+      flying: "#A98FF3",
+      psychic: "#F95587",
+      bug: "#A6B91A",
+      rock: "#B6A136",
+      ghost: "#735797",
+      dragon: "#6F35FC",
+      dark: "#705746",
+      steel: "#B7B7CE",
+      fairy: "#D685AD"
+    };
     const filters = alltypes.results
-      .map(type => `<button class="filter" data-name="${type.name}">${type.name}</button>`)
+      .map(type => `<button class="filter" data-name="${type.name}" style="background:${couleurs[type.name] || '#ccc'}">${type.name}</button>`)
       .join("");
 
     document.getElementById("filters").innerHTML = filters;
